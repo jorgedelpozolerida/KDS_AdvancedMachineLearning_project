c2c834020678671932fed4196a594eb2f8bdb347
thcla <tfc@tomzz.dk>
2023-04-23 07:34:17 +0200

    HPC files from HPC. OMG OMG OMG OMGgit status

---
+++ hpc/test_req.py
@@ -0,0 +1,31 @@
+import matplotlib
+print("matplotlib imported successfully!")
+
+import nibabel
+print("nibabel imported successfully!")
+
+import nilearn
+print("nilearn imported successfully!")
+
+import numpy
+print("numpy imported successfully!")
+
+import pandas
+print("pandas imported successfully!")
+
+import Pillow
+print("Pillow imported successfully!")
+
+import scikit_learn
+print("scikit_learn imported successfully!")
+
+import keras
+print("keras imported successfully!")
+
+import tensorflow
+print("tensorflow imported successfully!")
+
+import tqdm
+print("tqdm imported successfully!")
+
+print("ALL PACKAGES IMPORTED!")
